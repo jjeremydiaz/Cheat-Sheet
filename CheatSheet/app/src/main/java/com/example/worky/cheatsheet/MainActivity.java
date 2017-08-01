19a1cb0815ded9a235c8140d52ba2147da8ba8f1
Rohan Samtani <zapperrohan@gmail.com>
2017-08-01 13:18:48 -0700

    started project

--- CheatSheet/app/src/main/java/com/example/worky/cheatsheet/MainActivity.java
+++ CheatSheet/app/src/main/java/com/example/worky/cheatsheet/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.worky.cheatsheet;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+//import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+
+        Button loginButton = (Button) findViewById(R.id.button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
