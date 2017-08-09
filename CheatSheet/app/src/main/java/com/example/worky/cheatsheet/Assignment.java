e3df641966961e5852b010139c50d386ebf69719
Rohan Samtani <zapperrohan@gmail.com>
2017-08-09 10:03:10 -0700

    finished assignments and everything before

--- CheatSheet/app/src/main/java/com/example/worky/cheatsheet/Assignment.java
+++ CheatSheet/app/src/main/java/com/example/worky/cheatsheet/Assignment.java
@@ -1,5 +1,7 @@
 package com.example.worky.cheatsheet;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,11 @@ public class Assignment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assignment);
+
+        GlobalFunctions globalFunctions = new GlobalFunctions(this.getBaseContext(), this);
+        globalFunctions.changeTaskBarColor(R.color.homework);
+
+        android.support.v7.app.ActionBar bar = getSupportActionBar();
+        bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#03A9F4")));
     }
 }
