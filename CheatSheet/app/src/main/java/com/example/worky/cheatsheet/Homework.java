9899481e04769753db1082da7ca119a5f7b20001
Rohan Samtani <zapperrohan@gmail.com>
2017-08-09 09:11:05 -0700

    finsihed option page and started the homework feature, also finished all of the first few pages

--- CheatSheet/app/src/main/java/com/example/worky/cheatsheet/Homework.java
+++ CheatSheet/app/src/main/java/com/example/worky/cheatsheet/Homework.java
@@ -1,9 +1,12 @@
 package com.example.worky.cheatsheet;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Homework extends AppCompatActivity {
 
@@ -17,5 +20,36 @@ public class Homework extends AppCompatActivity {
 
         android.support.v7.app.ActionBar bar = getSupportActionBar();
         bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#03A9F4")));
+
+        Button loginButton = (Button) findViewById(R.id.Assignment1);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Intent intent = new Intent(getApplicationContext(), Assignment.class);
+                startActivity(intent);
+
+                Button loginButton = (Button) findViewById(R.id.Assignment2);
+                loginButton.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Code here executes on main thread after user presses button
+                        Intent intent = new Intent(getApplicationContext(), Assignment.class);
+                        startActivity(intent);
+
+                        Button loginButton = (Button) findViewById(R.id.Assignment3);
+                        loginButton.setOnClickListener(new View.OnClickListener() {
+                            public void onClick(View v) {
+                                // Code here executes on main thread after user presses button
+                                Intent intent = new Intent(getApplicationContext(), Assignment.class);
+                                startActivity(intent);
+
+                            }
+
+                        });
+
+                    }
+                });
+
+            }
+        });
     }
 }
